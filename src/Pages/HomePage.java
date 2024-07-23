20d972c0a48b244efde6f7ae8b577dcdbe55c80c
SudSad <ssadhukhan.007@gmail.com>
2024-07-23 16:36:22 +0530

    add/modify/delete

--- src/Pages/HomePage.java
+++ src/Pages/HomePage.java
@@ -3,6 +3,10 @@ package Pages;
 public class HomePage {
 	public static void main (String a[]) {
 		System.out.println("this is my Home Page");
+		String un = "Admin";
+		String pwd ="admin";
+		System.out.println("logged in...");
+				
 	}
 
 }
--- src/tests/LoginPageTest.java
+++
@@ -1,8 +0,0 @@
-package tests;
-
-public class LoginPageTest {
-	public void test1() {
-		 System.out.println("test1 - LP");
-	}
-
-}
